b37a62d2ea8aebbff6e5db23cd3ddf03d6e7a6b0
Joscha <neske.joscha@gmail.com>
2022-07-03 16:57:21 +0200

    footer icon changes

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -1,19 +1,19 @@
 import { FC } from 'react'
-import { FacebookIcon, InstagramIcon, TwitterIcon } from '../../helpers/icons/icons'
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
 
 interface FooterProps {}
 
 const icons = [
   {
-    icon: TwitterIcon,
+    icon: FaTwitter,
     link: 'www.google.de',
   },
   {
-    icon: FacebookIcon,
+    icon: FaFacebook,
     link: 'www.google.de',
   },
   {
-    icon: InstagramIcon,
+    icon: FaInstagram,
     link: 'www.google.de',
   },
 ]
@@ -23,13 +23,6 @@ const Footer: FC<FooterProps> = ({}) => {
     <footer className='bg-black'>
       <div className='max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8'>
         <nav className='-mx-5 -my-2 flex flex-wrap justify-center' aria-label='Footer'>
-          {/* {navigation.map((item) => (
-            <div key={item.name} className='px-5 py-2'>
-              <a href={item.href} className='text-base text-textcolor hover:text-gray-300 transition'>
-                {item.name}
-              </a>
-            </div>
-          ))} */}
           <div key='impressum' className='px-5 py-2'>
             <a href='/impressum' className='text-base text-textcolor hover:text-gray-300 transition'>
               Impressum
