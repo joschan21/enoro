6d1e334144c76b411a03b93509e0855d9474b009
Joscha <neske.joscha@gmail.com>
2022-07-15 15:48:51 +0200

    fix: specified maintenance mode matcher

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,13 @@
-// middleware.ts
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
   const { origin } = req.nextUrl
 
-  if (process.env.MAINTENANCE_MODE) return NextResponse.redirect(`${origin}/maintenance`)
+  if (process.env.MAINTENANCE_MODE === '1') return NextResponse.redirect(`${origin}/maintenance`)
   return NextResponse.next()
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: ['/', '/impressum', '/404', '/datenschutz'],
 }
