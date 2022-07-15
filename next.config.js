f95b96545e1a76a397686a37bb4bbb753815999f
Joscha <neske.joscha@gmail.com>
2022-07-15 15:47:26 +0200

    fix: redirects interfered with image paths

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: true,
-// })
-
 const nextConfig = {
-  async redirects() {
-    return [
-      process.env.MAINTENANCE_MODE === '1' && {
-        source: '/((?!maintenance).*)',
-        destination: '/maintenance',
-        permanent: false,
-      },
-    ].filter(Boolean)
-  },
   reactStrictMode: true,
   images: {
     domains: ['cdn.sanity.io'],
