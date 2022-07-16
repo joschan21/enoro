ca78da19ca969083018dee42b178162fd270d7d1
Joscha <neske.joscha@gmail.com>
2022-07-16 13:18:49 +0200

    fix: eslint minor change

--- components/homepage/LogoCloud.tsx
+++ components/homepage/LogoCloud.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image'
 import { FC } from 'react'
 
-interface LogoCloudProps {}
-
-const LogoCloud: FC<LogoCloudProps> = ({}) => {
+const LogoCloud: FC = () => {
   return (
     <div className='bg-darkblue'>
       <div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8'>
